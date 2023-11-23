4e99f771b5aa989916fea31bca03d3a6b26ed151
husicmirza <mirza.huc@gmail.com>
2023-11-23 11:15:13 +0100

    Created home page structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
-import Image from "next/image";
+import Camp from "@/components/Camp";
+import Features from "@/components/Features";
+import GetApp from "@/components/GetApp";
+import Guide from "@/components/Guide";
+import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col p-24">
-      <h1>Hello world</h1>
-    </main>
+    <>
+      <Hero />
+      <Camp />
+      <Guide />
+      <Features />
+      <GetApp />
+    </>
   );
 }
