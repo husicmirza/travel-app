741c2ac63780aad6c9c37ab03cab039bf9647169
husicmirza <mirza.huc@gmail.com>
2024-09-23 18:52:50 +0300

    Update next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  basePath: "/travel-app",
+  output: "export",
+  reactStrictMode: true,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
